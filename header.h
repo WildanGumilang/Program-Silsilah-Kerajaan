5bc0f1a6517b3d55ef17e1ccac2bffdbd12f1daa
Muhammad Wildan Gumilang <muhammadgumilang19@gmail.com>
2024-05-06 20:18:06 +0700

    first commit

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef siljaan_h
+#define siljaan_h
+
+#include <malloc.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <conio.h>
+#include <windows.h>
+#include "boolean.h"
+
+
+
+#endif
