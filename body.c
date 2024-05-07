956cb3c760fcc7454e24201f1413cafcbf7bbf55
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-05-07 10:31:26 +0700

    menambah fitur insert

--- body.c
+++ body.c
@@ -33,3 +33,37 @@ nbAddr searchNode(Root X,infotype nama)
 		}
 	}
 }
+
+void nbInsert(infotype n,bool Kl,int usia,nbAddr ortu)
+{
+    nbAddr temp;
+    nbAddr newnode;
+    newnode = (nbAddr)malloc(sizeof(TNode));
+    if (newnode != NULL)
+    {
+        printf("Error,alokasi gagal");
+        exit(1);
+    }
+        newnode->nama = n;
+        newnode->age = usia;
+        newnode->gender = Kl;
+        newnode->parent = ortu ->nama;
+        newnode->fs = NULL;
+        newnode->nb = NULL;
+        newnode->pr = ortu;
+        newnode->ps = NULL;
+
+        if (ortu->fs != NULL)
+        { 
+            temp = ortu->fs;
+            while (temp->nb != NULL)
+            {
+                temp = temp->nb;
+            }
+            temp ->nb = newnode;
+        }
+        else
+        {
+            ortu->fs = newnode;
+        }
+}
