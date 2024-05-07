fc98215380440a9cac65dd7e2569a5e812d522bc
Muhammad Wildan Gumilang <muhammadgumilang19@gmail.com>
2024-05-07 08:40:38 +0700

    create header

--- header.h
+++ header.h
@@ -11,6 +11,29 @@
 #include <windows.h>
 #include "boolean.h"
 
+typedef char* infotype;
+typedef struct royalNode *nbAddr;
+typedef struct mate *mateAddr;
+
+typedef struct mate{
+	infotype nama;
+	int age;
+	bool gender;
+}nbMateNode;
+
+typedef struct royalTree{
+	infotype nama;
+	int age;
+	bool gender;
+	nbAddr fs;
+	nbAddr nb;
+	nbAddr pr;
+	mateAddr ps;
+}nbTreeNode;
+
+struct nbTree{
+	nbAddr root;
+};
 
 
 #endif
